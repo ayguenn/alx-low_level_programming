70dd3880ec845a48d7bc9273680c6d5f0d2f2bd6
ayoub guennach <aguennach06@gmail.com>
2023-06-16 15:11:27 -0700

    updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ * prints the alphabet in lowercase
  * Return: Always 0 
  */
 int main(void)
